00a855c117398791ede9220648b84f7eb3d039a5
ThomasCl <56159161+ThomasCl@users.noreply.github.com>
2021-02-22 18:42:57 +0100

    laatsteoefofzo

--- 02-advanced-python/02-list-comprehensions/12-greatest-sum/student.py
+++ 02-advanced-python/02-list-comprehensions/12-greatest-sum/student.py
@@ -1,1 +1,12 @@
-# Write your code here
+def greatest_sum(ns):
+    winner = 0
+    ret = (0,0)
+    for i in range(ns):
+        for j in range(i, ns):
+            tot = 0
+            for k in range (i, j):
+                tot += ns[k]
+            if tot > winner:
+                winner = tot
+                ret = (i,j)
+    return ret
